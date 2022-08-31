f3865eca5a30fff722054da094eb8600402540a1
Daniel <schwartzray8@gmail.com>
2022-08-31 00:53:17 -0400

    migration

--- src/utils/migrateFromTaskDB.ts
+++ src/utils/migrateFromTaskDB.ts
@@ -3,29 +3,31 @@ import { Task } from "../entities/Task";
 import { Event } from "../entities/Event";
 
 export const migrateFromTaskDB = async () => {
-    const tasks = await Task.find();
+  const tasks = await Task.find();
 
-    // iterate through all users in taskDB
-    tasks.forEach(async (task: Task) => {
-        let weeklyGoalId = await WeeklyGoal.findOne({where: {discordId: task.discordId}})
-        if (!weeklyGoalId) {
-            // create weekly goal
-            weeklyGoalId = await WeeklyGoal.create({
-                discordId: task.discordId,
-                isActive: true,
-                description: task.description,
-                goalLeftChannelId: task.goalLeftChannelId,
-                misses: 0,
-            })
-        }
-        
-        let eventId = await Event.create({
-            discordId: task.discordId,
-            goalId: weeklyGoalId.id,
-            adjustedDate: task.date,
-            completed: task.completed,
-            isActive: true,
-            goalLeftChannelId: task.goalLeftChannelId
-        })
-    })
-}
+  // iterate through all users in taskDB
+  tasks.forEach(async (task: Task) => {
+    let weeklyGoalId = await WeeklyGoal.findOne({
+      where: { discordId: task.discordId },
+    });
+    if (!weeklyGoalId) {
+      // create weekly goal
+      weeklyGoalId = WeeklyGoal.create({
+        discordId: task.discordId,
+        isActive: true,
+        description: task.description,
+        goalLeftChannelId: task.goalLeftChannelId,
+        misses: 0,
+      });
+    }
+
+    Event.create({
+      discordId: task.discordId,
+      goalId: weeklyGoalId.id,
+      adjustedDate: task.date,
+      completed: task.completed,
+      isActive: true,
+      goalLeftChannelId: task.goalLeftChannelId,
+    });
+  });
+};
