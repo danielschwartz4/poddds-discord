ccec43d2287b51a97082334a25ce53e919994a35
KevinH48264 <1kevin.huang@gmail.com>
2022-09-06 20:22:58 -0400

    removed duplicate search in code

--- src/jobs/cleanActiveEvents.ts
+++ src/jobs/cleanActiveEvents.ts
@@ -8,7 +8,8 @@ export const cleanActiveEvents = async () => {
     // if the event's weekly goal is not active, set the event to not active
     allActiveGoals.forEach(async (active_goal: Event) => {
         let weeklyGoalMatch = await WeeklyGoal.findOne({ where: { id: active_goal.goalId}})
-        if (!weeklyGoalMatch || !weeklyGoalMatch?.isActive || (active_goal.goalId in checkedGoalIds && active_goal.adjustedDate in checkedGoalDate)) {
+        if (!weeklyGoalMatch || !weeklyGoalMatch?.isActive) {
+            console.log("THIS WEEKLY EVENT " + active_goal.id + " HAS A GOAL THAT ISN'T ACTIVE: " + active_goal.goalId)
             await Event.update({ id: active_goal.id }, { isActive: false })
         }
     })
@@ -16,12 +17,15 @@ export const cleanActiveEvents = async () => {
     // remove duplicates
     let duplicatesIds: number[] = [];
     allActiveGoals.forEach((active_goal: Event) => {
-        if (checkedGoalIds.includes(active_goal.goalId) && checkedGoalDate.includes(active_goal.adjustedDate)) {
-            duplicatesIds.push(active_goal.id)
-        } else {
-            // catch duplicates
-            checkedGoalIds.push(active_goal.goalId)
-            checkedGoalDate.push(active_goal.adjustedDate)
+        if (active_goal.isActive) {
+            if (checkedGoalIds.includes(active_goal.goalId) && checkedGoalDate.includes(active_goal.adjustedDate)) {
+                console.log("THIS ACTIVE GOAL IS A DUPLICATE, event id: " + active_goal.id)
+                duplicatesIds.push(active_goal.id)
+            } else {
+                // catch duplicates
+                checkedGoalIds.push(active_goal.goalId)
+                checkedGoalDate.push(active_goal.adjustedDate)
+            }
         }
     })
     duplicatesIds.forEach(async (goalId: number) => {
