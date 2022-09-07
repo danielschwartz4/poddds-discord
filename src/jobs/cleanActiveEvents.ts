5275d7b7762d6fda4dd472d8d789dfab759adbcc
KevinH48264 <1kevin.huang@gmail.com>
2022-09-06 21:00:12 -0400

    Adding code to update weekly goals to true

--- src/jobs/cleanActiveEvents.ts
+++ src/jobs/cleanActiveEvents.ts
@@ -1,34 +1,59 @@
 import { WeeklyGoal } from "../entities/WeeklyGoal";
 import { Event } from "../entities/Event";
+import { TODAY } from "../constants";
+import { addDays, mdyDate } from "../utils/timeZoneUtil";
 
 export const cleanActiveEvents = async () => {
     let allActiveGoals = await Event.find({ where: { isActive: true }, order: { id: "DESC" }})
-    let checkedGoalIds: number[] = [];
-    let checkedGoalDate: string[] = [];
+    // let checkedGoalIds: number[] = [];
+    // let checkedGoalDate: string[] = [];
+
+    
     // if the event's weekly goal is not active, set the event to not active
     allActiveGoals.forEach(async (active_goal: Event) => {
         let weeklyGoalMatch = await WeeklyGoal.findOne({ where: { id: active_goal.goalId}})
         if (!weeklyGoalMatch || !weeklyGoalMatch?.isActive) {
             console.log("THIS WEEKLY EVENT " + active_goal.id + " HAS A GOAL THAT ISN'T ACTIVE: " + active_goal.goalId)
             await Event.update({ id: active_goal.id }, { isActive: false })
+
         }
     })
 
-    // remove duplicates
-    let duplicatesIds: number[] = [];
-    allActiveGoals.forEach((active_goal: Event) => {
-        if (active_goal.isActive) {
-            if (checkedGoalIds.includes(active_goal.goalId) && checkedGoalDate.includes(active_goal.adjustedDate)) {
-                console.log("THIS ACTIVE GOAL IS A DUPLICATE, event id: " + active_goal.id)
-                duplicatesIds.push(active_goal.id)
-            } else {
-                // catch duplicates
-                checkedGoalIds.push(active_goal.goalId)
-                checkedGoalDate.push(active_goal.adjustedDate)
+    // // remove duplicates
+    // let duplicatesIds: number[] = [];
+    // allActiveGoals.forEach((active_goal: Event) => {
+    //     if (active_goal.isActive) {
+    //         if (checkedGoalIds.includes(active_goal.goalId) && checkedGoalDate.includes(active_goal.adjustedDate)) {
+    //             // console.log("THIS ACTIVE GOAL IS A DUPLICATE, event id: " + active_goal.id)
+    //             duplicatesIds.push(active_goal.id)
+    //         } else {
+    //             // catch duplicates
+    //             checkedGoalIds.push(active_goal.goalId)
+    //             checkedGoalDate.push(active_goal.adjustedDate)
+    //         }
+    //     }
+    // })
+    // duplicatesIds.forEach(async (goalId: number) => {
+    //     await Event.update({ id: goalId }, { isActive: false })
+    // })
+
+    // ENSURE THAT ALL WEEKLY GOALS HAVE ONE ACTIVE EVENT IN CASE ITS MARKED AS FALSE
+    // update not completed events today or after with isActive = false with highest id
+    let dates_array = []
+    for (let i = 0; i <= 28; i++) {
+        const date = addDays(TODAY, i);
+        const formattedDate = mdyDate(date);
+        dates_array.push(formattedDate)
+    }
+
+    let allActiveWeeklyGoals = await WeeklyGoal.find({ where: { isActive: true }, order: { id: "DESC" }})
+    dates_array.forEach((date: string) => {
+        allActiveWeeklyGoals.forEach(async (active_weekly_goal: WeeklyGoal) => {
+            // update top events based on date and active weekly goal id to true
+            let mostRecentEventForWeeklyGoal = await Event.findOne({where: { adjustedDate: date, goalId: active_weekly_goal.id}, order: { id: "DESC" }})
+            if (mostRecentEventForWeeklyGoal) {
+                await Event.update( { id: mostRecentEventForWeeklyGoal.id }, { isActive: true})
             }
-        }
-    })
-    duplicatesIds.forEach(async (goalId: number) => {
-        await Event.update({ id: goalId }, { isActive: false })
-    })
+        })
+    })   
 }
