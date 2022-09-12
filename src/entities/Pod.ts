cb43ce5ab380b28eafb4fd7a091b3972b0c4395c
Daniel <schwartzray8@gmail.com>
2022-09-12 02:06:05 -0400

    await auto kick

---
+++ src/entities/Pod.ts
@@ -0,0 +1,38 @@
+import { GoalType } from "../types/dbTypes";
+import { Field, Int, ObjectType } from "type-graphql";
+import {
+  BaseEntity,
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from "typeorm";
+
+@ObjectType()
+@Entity()
+export class Pod extends BaseEntity {
+  @Field(() => Int)
+  @PrimaryGeneratedColumn()
+  id!: number;
+
+  @Field()
+  @Column({
+    type: "enum",
+    enum: ["exercise", "study"],
+    default: "exercise",
+  })
+  type: GoalType;
+
+  @Field()
+  @Column({ default: 0 })
+  numMembers: number;
+
+  @Field()
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @Field()
+  @UpdateDateColumn()
+  updatedAt: Date;
+}
