f7d3aca463fab84c0d37540a171a1f2d77402178
KevinH48264 <1kevin.huang@gmail.com>
2022-10-27 17:47:16 -0400

    update expired notif

--- src/utils/channelUtil.ts
+++ src/utils/channelUtil.ts
@@ -82,7 +82,7 @@ export const createGoalsLeftTodayChannel = async (user: User, category_channel: 
     if (weekly_goal?.description) {
       var Difference_In_Time = weekly_goal.adjustedEndDate.setHours(0,0,0,0) - LOCAL_TODAY(timeZoneIsUTCMidnight as string).setHours(0,0,0,0);
       // To calculate the no. of days between two dates
-      var Difference_In_Days = Math.round(Difference_In_Time / (1000 * 3600 * 24));
+      var Difference_In_Days = Math.round(Difference_In_Time / (1000 * 3600 * 24)) + 1;
       let days_left_message = Difference_In_Days + " days left!";
       if (Difference_In_Days === 1) { days_left_message = "1 day left! 🏁 🏃‍♂️ " + `<@${user.discordId}>` }
       if (Difference_In_Days < 1) { expiredGoalNotif(user.discordId, podType, weekly_goal) } // backup code in case expired goal notif doesn't trigger somehow
